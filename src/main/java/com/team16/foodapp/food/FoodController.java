c943e1702bbe6bf659c1bb0294b53914ae817a33
Yujin <jyj9747@naver.com>
2021-12-17 19:07:51 +0900

    12/17

---
+++ src/main/java/com/team16/foodapp/food/FoodController.java
@@ -0,0 +1,63 @@
+package com.team16.foodapp.food;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class FoodController {
+
+	@Autowired
+	FoodDAO foodDAO ;
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/food/list", method = RequestMethod.GET)
+	public String foodList(Model model) {
+		model.addAttribute("list", foodDAO.getFoodList());
+		return "list" ;
+	}
+	
+	@RequestMapping(value = "/food/add", method = RequestMethod.GET)
+	public String addFood() {
+		return "addfoodform" ;
+	}
+	
+	@RequestMapping(value = "/food/addok", method = RequestMethod.POST)
+	public String addFoodOk(FoodVO vo) {
+		int i = foodDAO.insertFood(vo) ;
+		if(i==0) System.out.println("데이터 추가 실패!") ;
+		else System.out.println("데이터 추가 성공!") ;
+		return "redirect:list" ;
+	}
+	
+	@RequestMapping(value = "/food/editfood/{id}", method = RequestMethod.GET)
+	public String editPost(@PathVariable("id") int id, Model model) {
+		FoodVO foodVO = foodDAO.getFood(id) ;
+		model.addAttribute("foodVO", foodVO) ;
+		return "editform" ;
+	}
+	
+	@RequestMapping(value = "/food/editok", method = RequestMethod.POST)
+	public String editFoodOk(FoodVO vo) {
+		int i = foodDAO.updateFood(vo) ;
+		if(i==0) System.out.println("데이터 수정 실패!") ;
+		else System.out.println("데이터 수정 성공!") ;
+		return "redirect:list" ;
+	}
+	
+	
+	@RequestMapping(value = "/food/deleteok/{id}", method = RequestMethod.GET)
+	public String deleteFood(@PathVariable("id") int id) {
+		int i = foodDAO.deleteFood(id) ;
+		if(i==0) System.out.println("데이터 삭제 실패!") ;
+		else System.out.println("데이터 삭제 성공!") ;
+		return "redirect:../list" ;
+	}
+	
+
+}
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -10,6 +10,6 @@
 </h1>
 
 <P>  The time on the server is ${serverTime}. </P>
-<p><a href="food/list">음식 재고 관리 application으로 넘어가기</a></p>
+<p><a href="list">음식 재고 관리 application으로 넘어가기</a></p>
 </body>
 </html>
