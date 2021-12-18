fe9f6531f9fc521b6fead1086846ffa6a50ef3d0
yujin9747 <89755770+yujin9747@users.noreply.github.com>
2021-12-18 13:17:53 +0900

    Update FoodDAO.java

--- src/main/java/com/team16/foodapp/food/FoodDAO.java
+++ src/main/java/com/team16/foodapp/food/FoodDAO.java
@@ -1,5 +1,6 @@
 package com.team16.foodapp.food;
 
+import java.util.Calendar;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
@@ -12,6 +13,9 @@ public class FoodDAO {
 	SqlSession sqlSession; 
 	
 	public int insertFood(FoodVO vo) {
+		String date = vo.getExpirationdate() ;
+		String[] arr = date.split("/") ;
+		if(arr.length != 3) return 0 ;
 		int result = sqlSession.insert("Food.insertFood", vo) ;
 		return result ;
 	}
@@ -35,4 +39,30 @@ public class FoodDAO {
 		List<FoodVO> list = sqlSession.selectList("Food.getFoodList") ;
 		return list ;
 	}
+	
+	public int dDaynotification(FoodVO vo) {
+		int d_Day = 0;
+		
+		//비교할 날짜
+	    Calendar target = Calendar.getInstance();
+	    String date = vo.getExpirationdate() ;
+	    String[] arr = date.split("/") ;
+	    target.set(Integer.parseInt(arr[0]), Integer.parseInt(arr[1])-1, Integer.parseInt(arr[2]));
+	      
+	      // 현재 날짜
+	    Calendar present = Calendar.getInstance();
+	       
+	      // 5. D- Day 계산
+	     long time =  (target.getTimeInMillis() - present.getTimeInMillis())/(24*60*60*1000);
+	     d_Day = (int) time;
+	     if(d_Day < 0) sqlSession.update("Food.setExpired", vo) ; 
+	     return d_Day ;
+	       
+	  }
+
+	public int completeFood(FoodVO vo) {
+		int result = sqlSession.update("Food.completeFood", vo) ;
+		return result ;
+	}
 }
+
